22b8a161b33e77b58e30016249d1f6b1ae386134
Dan Rusu <roosterdan@gmail.com>
2023-07-22 19:08:53 -0600

    Create initial project structure

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") version libs.versions.kotlin
+    `jvm-test-suite`
+}
+
+group = "org.rusu.tax-calculator"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+        allWarningsAsErrors.set(true)
+    }
+}
+
+plugins.withId("org.jetbrains.kotlin.jvm") {
+    dependencies {
+        testImplementation(libs.strikt)
+        testRuntimeOnly(libs.jUnitPlatformLauncher)
+    }
+    testing {
+        suites {
+            val test by getting(JvmTestSuite::class) {
+                useJUnitJupiter(libs.versions.jUnit)
+            }
+        }
+    }
+}
