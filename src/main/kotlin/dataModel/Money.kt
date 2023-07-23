15b989809fc38a797074628c018a67c9e09a49cf
Dan Rusu <roosterdan@gmail.com>
2023-07-22 21:12:37 -0600

    Define Money data model

---
+++ src/main/kotlin/dataModel/Money.kt
@@ -0,0 +1,41 @@
+package dataModel
+
+private const val CENTS_PER_DOLLAR = 100L
+
+@JvmInline
+value class Money private constructor(val cents: Long) {
+    init {
+        require(cents >= 0) { "Negative cents are not allowed: $cents" }
+    }
+
+    operator fun plus(amount: Money): Money = Money(cents = this.cents + amount.cents)
+
+    operator fun minus(amount: Money): Money = Money(cents = this.cents - amount.cents)
+
+    operator fun times(amount: Long): Money = Money(cents = this.cents * amount)
+
+    operator fun div(amount: Long): Money = Money(cents = this.cents / amount)
+
+    operator fun compareTo(other: Money): Int = cents.compareTo(other.cents)
+
+    companion object {
+        fun ofCents(amount: Long): Money = Money(cents = amount)
+
+        fun ofDollars(amount: Long): Money = Money(cents = amount * CENTS_PER_DOLLAR)
+
+        fun of(dollars: Long, cents: Int): Money {
+            require(dollars >= 0) { "Negative dollars are not allowed: $dollars" }
+            require(cents in 0L..99L) { "Cents must be between 0 and 99 inclusive" }
+
+            return Money(cents = dollars * CENTS_PER_DOLLAR + cents)
+        }
+
+        fun of(dollars: Int, cents: Int): Money = of(dollars.toLong(), cents)
+
+        val Int.dollars: Money
+            get() = ofDollars(this.toLong())
+
+        val Int.cents: Money
+            get() = ofCents(this.toLong())
+    }
+}
---
+++ src/test/kotlin/dataModel/MoneyTest.kt
@@ -0,0 +1,132 @@
+package dataModel
+
+import dataModel.Money.Companion.cents
+import dataModel.Money.Companion.dollars
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.api.expectThrows
+import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
+import strikt.assertions.message
+
+class MoneyTest {
+    @Test
+    fun `cannot create negative money amounts`() {
+        expectThrows<IllegalArgumentException> {
+            Money.ofCents(-10L)
+        }.message.isEqualTo("Negative cents are not allowed: -10")
+
+        expectThrows<IllegalArgumentException> {
+            (-10).cents
+        }.message.isEqualTo("Negative cents are not allowed: -10")
+
+        expectThrows<IllegalArgumentException> {
+            Money.ofDollars(-5L)
+        }.message.isEqualTo("Negative cents are not allowed: -500")
+
+        expectThrows<IllegalArgumentException> {
+            (-5).dollars
+        }.message.isEqualTo("Negative cents are not allowed: -500")
+
+        expectThrows<IllegalArgumentException> {
+            Money.of(dollars = -3, cents = 25)
+        }.message.isEqualTo("Negative dollars are not allowed: -3")
+
+        expectThrows<IllegalArgumentException> {
+            Money.of(dollars = 10, cents = -1)
+        }.message.isEqualTo("Cents must be between 0 and 99 inclusive")
+
+        expectThrows<IllegalArgumentException> {
+            Money.of(dollars = 10, cents = 100)
+        }.message.isEqualTo("Cents must be between 0 and 99 inclusive")
+    }
+
+    @Test
+    fun `adding money`() {
+        expectThat(7.dollars + 0.dollars)
+            .isEqualTo(7.dollars)
+
+        expectThat(7.dollars + 0.cents)
+            .isEqualTo(7.dollars)
+
+        expectThat(3.dollars + 5.dollars)
+            .isEqualTo(8.dollars)
+
+        expectThat(50.cents + 50.cents)
+            .isEqualTo(1.dollars)
+
+        expectThat(3.dollars + 25.cents)
+            .isEqualTo(Money.of(dollars = 3, cents = 25))
+    }
+
+    @Test
+    fun `subtracting money`() {
+        expectThat(7.dollars - 0.dollars)
+            .isEqualTo(7.dollars)
+
+        expectThat(7.dollars - 0.cents)
+            .isEqualTo(7.dollars)
+
+        expectThat(5.dollars - 3.dollars)
+            .isEqualTo(2.dollars)
+
+        expectThat(2.dollars - 2.dollars)
+            .isEqualTo(0.dollars)
+
+        expectThat(3.dollars - 50.cents)
+            .isEqualTo(Money.of(dollars = 2, cents = 50))
+    }
+
+    @Test
+    fun `subtraction cannot result in negative money`() {
+        expectThrows<IllegalArgumentException> {
+            2.dollars - 5.dollars
+        }.message.isEqualTo("Negative cents are not allowed: -300")
+    }
+
+    @Test
+    fun `multiplying money`() {
+        expectThat(3.dollars * 5)
+            .isEqualTo(15.dollars)
+
+        expectThat(50.cents * 0)
+            .isEqualTo(0.cents)
+    }
+
+    @Test
+    fun `dividing money`() {
+        expectThat(6.dollars / 2)
+            .isEqualTo(3.dollars)
+
+        expectThat(5.dollars / 4)
+            .isEqualTo(Money.of(dollars = 1, cents = 25))
+    }
+
+    @Test
+    fun `cents are rounded down during division`() {
+        expectThat(29.cents / 10)
+            .isEqualTo(2.cents)
+    }
+
+    @Test
+    fun `comparing money`() {
+        expectThat(4.dollars == 4.dollars)
+            .isTrue()
+
+        expectThat(4.dollars != 4.dollars)
+            .isFalse()
+
+        expectThat(3.dollars == 7.dollars)
+            .isFalse()
+
+        expectThat(5.dollars == 5.cents)
+            .isFalse()
+
+        expectThat(3.dollars > 2.dollars)
+            .isTrue()
+
+        expectThat(Money.of(dollars = 5, cents = 1) < 5.dollars)
+            .isFalse()
+    }
+}
