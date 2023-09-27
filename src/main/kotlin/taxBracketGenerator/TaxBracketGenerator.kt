d3f994cef38e534bb20ae9f8b90474ac6b8e6ebd
Dan Rusu <roosterdan@gmail.com>
2023-09-27 11:29:53 -0600

    Create TaxBracketGenerator

---
+++ src/main/kotlin/taxBracketGenerator/TaxBracketGenerator.kt
@@ -0,0 +1,73 @@
+package taxBracketGenerator
+
+import dataModel.Money.Companion.dollars
+import dataModel.Percent
+import dataModel.Percent.Companion.percent
+import dataModel.TaxBracket
+import kotlin.random.Random
+
+private val maxPercentBps = 100.percent.amountBps
+
+object TaxBracketGenerator {
+    /**
+     * The current data model can support a maximum of 10,000 brackets due to the following reasons:
+     *
+     * 1. In a progressive income tax system, the tax rate must be strictly increasing as you advance to the next
+     * bracket.  This results in an ordered list of brackets starting with the lowest tax rate and ending in the
+     * highest tax rate.
+     *
+     * 2. Consecutive brackets cannot have the same tax rate otherwise they would be part of a single larger tax
+     * bracket.
+     *
+     * 3. All brackets need a different tax rate in order to fulfill the previous 2 requirements so the maximum number
+     * of brackets is limited by the maximum number of unique tax rates.
+     *
+     * 4. All regions in the world limit tax bracket percentages to at most 2 decimal places so using basis points is
+     * the granularity limit.
+     *
+     * 5. Each percent is split into 100bps so there are up to 100% X 100bps = 10,000 unique tax rates possible.
+     */
+    val maxNumTaxBrackets = maxPercentBps.toInt()
+
+    fun generateTaxBrackets(
+        numBrackets: Int,
+        lowerBoundDollarsOfHighestBracket: Int = numBrackets * 100,
+        random: Random = Random,
+    ): List<TaxBracket> {
+        require(numBrackets > 0) { "The number of tax brackets must be positive" }
+        require(numBrackets <= maxNumTaxBrackets) { "Cannot create more than $maxNumTaxBrackets tax brackets" }
+        if (numBrackets == 1) {
+            require(lowerBoundDollarsOfHighestBracket == 0) {
+                "The lower bound of the highest bracket must be 0 when creating a single bracket"
+            }
+        }
+        require(lowerBoundDollarsOfHighestBracket + 1 >= numBrackets) {
+            "Cannot create $numBrackets brackets if the highest bracket starts at $lowerBoundDollarsOfHighestBracket"
+        }
+
+        // Divide the range into numBrackets distinct lower-bound values including 0
+        val lowerBounds = hashSetOf(0.dollars)
+        lowerBounds += lowerBoundDollarsOfHighestBracket.dollars
+        while (lowerBounds.size < numBrackets) {
+            lowerBounds += random.nextInt(until = lowerBoundDollarsOfHighestBracket).dollars
+        }
+
+        // generate a unique percentage for each bracket up to 100.00% (exclusive)
+        val percentages = hashSetOf<Percent>()
+        while (percentages.size < numBrackets) {
+            percentages += Percent.ofBasisPoints(random.nextLong(until = maxPercentBps))
+        }
+        val sortedPercentages = percentages.sortedBy { it.amountBps }
+        val sortedLowerBounds = lowerBounds.sortedBy { it.cents }
+
+        val result = ArrayList<TaxBracket>(numBrackets)
+        for (i in 0..<numBrackets) {
+            result += TaxBracket(
+                taxRate = sortedPercentages[i],
+                from = sortedLowerBounds[i],
+                to = sortedLowerBounds.getOrNull(i + 1)
+            )
+        }
+        return result
+    }
+}
---
+++ src/test/kotlin/taxBracketGenerator/TaxBracketGeneratorTest.kt
@@ -0,0 +1,138 @@
+package taxBracketGenerator
+
+import dataModel.Money.Companion.dollars
+import dataModel.Percent.Companion.percent
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.api.expectThrows
+import strikt.assertions.*
+import kotlin.random.Random
+
+class TaxBracketGeneratorTest {
+    @Test
+    fun `the number of tax brackets must be positive`() {
+        expectThrows<IllegalArgumentException> {
+            TaxBracketGenerator.generateTaxBrackets(numBrackets = -1)
+        }.message.isEqualTo("The number of tax brackets must be positive")
+
+        expectThrows<IllegalArgumentException> {
+            TaxBracketGenerator.generateTaxBrackets(numBrackets = 0)
+        }.message.isEqualTo("The number of tax brackets must be positive")
+
+        // positive # of brackets completes without failure
+        expectThat(TaxBracketGenerator.generateTaxBrackets(numBrackets = 3))
+            .hasSize(3)
+    }
+
+    @Test
+    fun `cannot create more than 10,000 brackets`() {
+        expectThat(TaxBracketGenerator.maxNumTaxBrackets)
+            .isEqualTo(10_000)
+
+        expectThrows<IllegalArgumentException> {
+            TaxBracketGenerator.generateTaxBrackets(numBrackets = 10_001)
+        }.message.isEqualTo("Cannot create more than 10000 tax brackets")
+
+        // Creating exactly the maximum completes without failure
+        expectThat(TaxBracketGenerator.generateTaxBrackets(numBrackets = 10_000))
+            .hasSize(10_000)
+    }
+
+    @Test
+    fun `the highest bracket must start at 0 when creating a single bracket`() {
+        expectThrows<IllegalArgumentException> {
+            TaxBracketGenerator.generateTaxBrackets(numBrackets = 1, lowerBoundDollarsOfHighestBracket = 1)
+        }.message.isEqualTo("The lower bound of the highest bracket must be 0 when creating a single bracket")
+
+        val brackets = TaxBracketGenerator.generateTaxBrackets(numBrackets = 1, lowerBoundDollarsOfHighestBracket = 0)
+        expectThat(brackets)
+            .hasSize(1)
+    }
+
+    @Test
+    fun `lower bound must be large enough to support the number of brackets`() {
+        expectThrows<IllegalArgumentException> {
+            TaxBracketGenerator.generateTaxBrackets(numBrackets = 2, lowerBoundDollarsOfHighestBracket = 0)
+        }.message.isEqualTo("Cannot create 2 brackets if the highest bracket starts at 0")
+
+        expectThrows<IllegalArgumentException> {
+            TaxBracketGenerator.generateTaxBrackets(numBrackets = 3, lowerBoundDollarsOfHighestBracket = 1)
+        }.message.isEqualTo("Cannot create 3 brackets if the highest bracket starts at 1")
+
+        // Allowing just enough range completes without failure
+        val brackets = TaxBracketGenerator.generateTaxBrackets(numBrackets = 4, lowerBoundDollarsOfHighestBracket = 3)
+        expectThat(brackets)
+            .hasSize(4)
+
+        for (index in brackets.indices) {
+            val bracket = brackets[index]
+            expectThat(bracket.from)
+                .isEqualTo(index.dollars)
+        }
+    }
+
+    @Test
+    fun `brackets have proper structure`() {
+        val seed = Random.nextLong()
+        val random = Random(seed)
+        println("Seed: $seed") // to be able to reproduce when encountering a failure
+
+        repeat(100) {
+            generateRandomBracketsAndValidateStructure(random)
+        }
+    }
+
+    private fun generateRandomBracketsAndValidateStructure(random: Random) {
+        val numBrackets = random.nextInt(1, 100)
+        val lowerBoundDollarsOfHighestBracket = when (numBrackets) {
+            1 -> 0
+            else -> random.nextInt(numBrackets - 1, numBrackets * 100)
+        }
+
+        val brackets = TaxBracketGenerator.generateTaxBrackets(
+            numBrackets = numBrackets,
+            lowerBoundDollarsOfHighestBracket = lowerBoundDollarsOfHighestBracket,
+            random = random
+        )
+        expectThat(brackets)
+            .hasSize(numBrackets)
+
+        with(brackets.first()) {
+            expectThat(from)
+                .isEqualTo(0.dollars)
+
+            expectThat(taxRate.amountBps)
+                .isGreaterThanOrEqualTo(0)
+        }
+
+        for (i in brackets.indices) {
+            val bracket = brackets[i]
+            val nextBracket = brackets.getOrNull(i + 1)
+
+            if (nextBracket != null) {
+                // lower bounds are strictly increasing
+                expectThat(bracket.from.cents)
+                    .isLessThan(nextBracket.from.cents)
+
+                // upper bound matches the lower bound of the next bracket
+                expectThat(bracket.to)
+                    .isEqualTo(nextBracket.from)
+
+                // tax rates are strictly increasing
+                expectThat(bracket.taxRate.amountBps)
+                    .isLessThan(nextBracket.taxRate.amountBps)
+            }
+        }
+
+        with(brackets.last()) {
+            expectThat(from)
+                .isEqualTo(lowerBoundDollarsOfHighestBracket.dollars)
+
+            expectThat(to)
+                .isNull()
+
+            expectThat(taxRate.amountBps)
+                .isLessThan(100.percent.amountBps)
+        }
+    }
+}
